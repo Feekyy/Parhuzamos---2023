651dfef1c20d47b845821d8317d3c6815363c44a
Vitkolczi Dániel <90444730+Feekyy@users.noreply.github.com>
2023-03-22 19:41:18 +0100

    Add files via upload
    még a 9-es feladat hiányzott az 1-es feladatok közül.

--- Gyak1/feladat9.c
+++ Gyak1/feladat9.c
@@ -7,19 +7,23 @@ void genFileRandInt(char filname[], int length);
 int main()
 {
     char name[] = {'a', 's', 'd', '.', 't', 'x', 't'};
-    genFileRandInt(name, 13);
+    genFileRandInt(name, 1000000);
 }
 
 void genFileRandInt(char filename[], int length)
 {
+    clock_t begin1 = clock();
     FILE *file = fopen(filename, "wr");
     srand(time(NULL));
-    clock_t begin = clock()
+    clock_t begin2 = clock();
     for (int i = 0; i < length; i++)
     {
         int r = rand();
         fprintf(file, "%d\n", r);
     }
-    clock_t begin = clock()
-    close(file)
+    clock_t stop2 = clock();
+    printf("The time i wasted for generating random numbers: %f\n", (double)(stop2 - begin2) / CLOCKS_PER_SEC);
+    fclose(file);
+    clock_t stop1 = clock();
+    printf("The time i wasted for creating the file: %f\n", (double)(stop1 - begin1) / CLOCKS_PER_SEC);
 }
