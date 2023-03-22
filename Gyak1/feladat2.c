1265d51a780f1feba0234a81a218e4879e1e7d55
Vitkolczi Dániel <90444730+Feekyy@users.noreply.github.com>
2023-03-22 19:42:20 +0100

    Update feladat2.c
    2-es feladatat újragondolva

--- Gyak1/feladat2.c
+++ Gyak1/feladat2.c
@@ -1,47 +1,25 @@
 #include <stdio.h>
-#include <stdbool.h>
 
-int main ()
+int main()
 {
-   int input, count = 0, alt, j;
-   bool ok = false;
-   char number1[8];
-   char number2[8];
-   int numberArray[count];
-
-   do
-   {
-     printf("Enter A maximal 8 length number: ");
-     scanf("%d", &input);
-     if (input < 100000000)
+    long long int input;
+    int ok = 0;
+    do
+    {
+        printf("Enter a number with max. 8 digits: ");
+        scanf("%lld", &input);
+        if (input < 100000000)
         {
-          ok = true;
+            ok = 1;
         }
-   } while (!ok);
+    } while (!ok);
 
-   while (input != 0)
-   {
-     numberArray[count] = input % 10;
-     input /= 10;
-     count++;
-   }
-   alt = 8 - count;
-   for (int i = 8; i > -1; i--)
-   {
-     if (i > alt)
-     {
-       number1[i] = numberArray[count]+'0';
-       number2[i] = numberArray[count]+'0';
-       count--;
-     }
-     else
-     {
-       number1[i] = ' ';
-       number2[i] ='0';
-     }
-   }
+    while (input > 100000000)
+    {
+       input /= 10;
+    }
+    printf("%08lld\n", input);
+    printf("%8lld", input);
 
-   printf("The nubmer after scanning and start with spaces: %s\n", number1);
-   printf("The nubmer after scanning and start with zeros: %s\n", number2);
-   return(0);
+    return 0;
 }
